933eed9b0d717b751821642a56aa42da1ab30ef2
Suvrat <suvratmittal007@gmail.com>
2024-08-17 17:11:04 +0530

    installed npm package

---
+++ App.js
@@ -0,0 +1,11 @@
+
+const parent = React.createElement  ("div" , {id: "parent"},
+    React.createElement("div" , {id:"child"},[
+        React.createElement("h1", {}," Im batman"),
+        React.createElement("h2", {}," Im ironman"),
+    ])
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
