201c4169c056b510f3661a895dc7de6ec372e9fb
Suvrat <suvratmittal007@gmail.com>
2024-08-21 00:13:28 +0530

    ep3

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement  ("div" , {id: "parent"},
-    React.createElement("div" , {id:"child"},[
-        React.createElement("h1", {}," Im batman"),
-        React.createElement("h2", {}," Im ironman"),
-    ])
-);
+// const jsxHeader = <h1 id="root">namaste vro</h1>;
 
+
+// const duntion = () => <><h1>react</h1>
+// <h2>reastion</h2></>
+// ;
+
+
+// const fun = () => (
+//     <><duntion />
+//     <h1>composite funtion</h1></>
+
+// )
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<duntion/>);
+
+const Hello = () => {
+    return <h1>HELLO WORLD</h1>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<Hello/>);
