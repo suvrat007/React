ece2e48d0868ba76cf162d112a01feaa1be8ea58
Suvrat <suvratmittal007@gmail.com>
2024-09-09 22:03:05 +0530

    namaste react ep 6 notes

---
+++ src/components/Shimmer.js
@@ -0,0 +1,25 @@
+import Header from "./Header";
+
+const shimmer = () =>{
+    return (
+        <div className="shimmer-container">
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+            <div className="shimmer-card"></div>
+        </div>
+    );
+};
+
+export default shimmer();
