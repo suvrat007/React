ad75d3c71df8cb0f32e7e2122fbcd55a1ae1e577
Suvrat <suvratmittal007@gmail.com>
2024-09-28 23:43:00 +0530

    namaste react ep 8 notes

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,46 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import {Component} from "react";
 
-const About = () =>{
-    return(
-        <div>
-            <h1>About</h1>
-            <h2>this is the about section</h2>
-            {/*<User />*/}
-            <UserClass name={"singham"} location={"kaali pahadi"} />
 
-        </div>
-    )
-};
+class About extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    //this is called after the class component was completely mounted onto the DOM
+
+    componentDidMount() {
+        // this is called after .... constructor and render
+
+        //componentDidMount is used to make API calls
+
+        // this is used bcoz its called at last....so PI call is amde after the component is rendered and mounted
+        // so we want to render first and then call the API instead of other way around
+
+
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>About</h1>
+                <h2>this is the about section</h2>
+                {/*<User />*/}
+
+                {/*when there are 2 children
+                    - first child is called....uska constructor and render is called
+                    - second child is then called ....uska constructor and render called
+                    - then dono ke DidMount is called repectively
+                 this is an optimization of react and makes it more efficient*/}
+
+                <UserClass name={"singham"} location={"kaali pahadi"} />
+
+            </div>
+        )
+    }
+}
+
+
 
 export default About;
